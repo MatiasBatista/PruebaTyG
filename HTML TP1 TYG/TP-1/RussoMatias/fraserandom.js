8708bdf75e47c45f9365a0d0dbe50bce211cf753
Matias Batista <matiasomarbatista99@gmail.com>
2022-05-28 19:57:26 -0300

    borro todos los archivos

--- HTML TP1 TYG/TP-1/RussoMatias/fraserandom.js
+++
@@ -1,9 +0,0 @@
-var randomN = Math.floor(Math.random()*(4-1))+1;
-
-if (randomN == "1") {
-	document.write('<p id="frase1" align = center>"El que rie ultimo, no entendio la broma."</p>');
-} else if (randomN == "2") {
-	document.write('<p id="frase2" align = center>"Rakan ama los chocolates"</p>');
-} else{
-	document.write('<p id="frase3" align = center>"Esta seria mi tercer frase. Jaja saludos"</p>');
-}
